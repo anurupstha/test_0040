7da35a02932b0007c4bc7e2c0d6e3284cb65500a
anurupstha <53064464+anurupstha@users.noreply.github.com>
2024-02-04 12:19:42 -0500

    terraform State

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-TODO
+# summarize the main objective of the main.tf code into paragraph
+
+The main objective of the `main.tf` file is to configure and manage infrastructure resources using Terraform, a popular open-source Infrastructure as Code (IaC) tool via github actions.
--- main.tf
+++ main.tf
@@ -29,48 +29,14 @@ resource "aws_instance" "myec2" {
     Name = "myec2"
   }
 }
-# Deploy Windows Server 2022 EC2 instance
-resource "aws_instance" "windows_instance" {
-  ami = "ami-00d990e7e5ece7974" # replace with your Windows Server 2022 AMI ID
-  instance_type = "t2.micro"
-  key_name = "terraform"
-  vpc_security_group_ids = [ "sg-0c2ac51aa7d6f3522" ]
-  user_data = <<-EOF
-              <powershell>
-              Install-WindowsFeature -name Web-Server -IncludeManagementTools
-              New-Item -Path 'C:\inetpub\wwwroot' -ItemType Directory -Force
-              Add-Content -Path 'C:\inetpub\wwwroot\index.html' -Value '<h1>Hello, Windows World!</h1>'
-              </powershell>
-              EOF
-  tags = {
-    Name = "WindowsInstance"
+#Terraform state locking using DynamoDB and S3 bucket
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-github-actions"
+    key    = "githubactions/project-demo/terraform.tfstate"
+    encrpt = true
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locking-github"
   }
-}
-
-#Create S3 bucket
-
-resource "aws_s3_bucket" "mybucket-anurup1" {
-  bucket = "mybucket-anurup1"
-  tags = {
-    Name = "mybucket-anurup1"
-  }
-}  
-
-# Create EBS volume
-
-resource "aws_ebs_volume" "myebs" {
-  availability_zone = "us-east-1a"
-  size = 10
-  tags = {
-    Name = "myebs"
-  }
-}
-
-# Create EBS volume attachment
-
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id = aws_ebs_volume.myebs.id
-  instance_id = aws_instance.myec2.id
-  force_detach = true
 }
